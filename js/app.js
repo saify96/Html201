27b94c12b99dd53f483f4340a10b7f2c6f4e963c
saify96 <mohammad.alsaify@gmail.com>
2021-04-04 14:43:00 +0300

    crypto lab1

---
+++ js/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let userName = prompt ("What is your name?");
+let userAge = prompt ("How old are you?");
+let survey = prompt ("What is your favourite platform?" , "Binance , Coinbase");
+let favCurrency = prompt ("What is your favorite currency?" , "BTC , ETH , BNB");
+
+alert ('Your name is ' + userName + ' and your age is ' + userAge + ' you favourite artist is ' + survey + ' And your favourite currency is ' + favCurrency);
